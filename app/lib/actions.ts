df190e7538d951a6712dfd26ff99ae053fb59c63
WRainerFlores <rainerneno@gmail.com>
2024-05-07 18:35:30 +0800

    added Update function for Invoices.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,6 +17,9 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 // export async function createInvoice(formData: FormData) {
 //     const rawFormData = {
 //         customerId: formData.get('customerId'),
@@ -50,5 +53,26 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
 
     // redirect the user back to the /dashboard/invoices page
+    redirect('/dashboard/invoices');
+}
+
+
+// Update Invoice
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql `
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+    
+    revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
