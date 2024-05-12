ddd57177658d7e5a159a6f69d6459d04dd8e0315
WRainerFlores <rainerneno@gmail.com>
2024-05-13 00:46:28 +0800

    added metdata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,23 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { ThemeProvider } from 'next-themes';
+import { Metadata } from 'next';
+
+// export const metadata: Metadata = {
+//   title: 'Acme Dashboard',
+//   description: 'The official Next.js Course Dashboard, built with App Router.',
+//   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+// };
+
+//use the title.template field in the metadata object to define a template for your page titles. The %s in the template will be replaced with the specific page title.
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout ( {
   children,
